4b30951e7afe31b8c0cfacb63cbd400d4c190bce
Amangoua Kacou Ange-Thierry <104840429+amangouste225@users.noreply.github.com>
2024-07-31 17:11:14 +0000

    structure the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
 export const App = () => {
-  return <div> Hello World i</div>;
+  return <RouterProvider router={router} />;
 };
